320175263455dcf0bbac0d4ffb6415f1371e1abe
P-Chan <hello@0x50.io>
2022-05-01 07:16:04 +0900

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# @stardust-configs/eslint-config
+
+> Shareable ESLint config
+
+## Install
+
+```sh
+npm install @stardust-configs/eslint-config -D
+```
+
+or
+
+```sh
+yarn add @stardust-configs/eslint-config -D
+```
+
+## Usage
+
+Edit `.eslintrc.js`.
+
+<details>
+
+<summary>Next.js</summary>
+
+```json
+{
+  "extends": ["@stardust-configs/eslint-config/next"]
+}
+```
+
+</details>
+
+## Author
+
+[@p-chan](https://github.com/p-chan)
+
+## License
+
+MIT
---
+++ next.js
@@ -0,0 +1,25 @@
+/**
+ * @type {import('eslint/lib/shared/types').ConfigData}
+ */
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:node/recommended',
+    'plugin:react/recommended',
+    'plugin:@next/next/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  ignorePatterns: ['./.next/**/*'],
+}
