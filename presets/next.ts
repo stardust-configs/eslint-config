76a24d391c2c002f1a21ae2566eb7b992d4b1c78
P-Chan <hello@0x50.io>
2023-03-17 05:21:01 +0900

    feat: add jsonc/sort-array-values and jsonc/sort-keys to package.json properties

--- presets/next.ts
+++ presets/next.ts
@@ -3,6 +3,7 @@ export = {
     '../environments/browser',
     '../environments/node',
     '../environments/es2022',
+    '../configs/npm',
     '../configs/typescript',
     '../configs/react',
     '../configs/next',
