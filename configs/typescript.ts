56140e472aba10656283f6793fa1e1b3eacb98c9
P-Chan <hello@0x50.io>
2023-09-24 00:51:04 +0900

    refactor: move import rules to es6
    import is an es6 syntax

--- configs/typescript.ts
+++ configs/typescript.ts
@@ -2,44 +2,11 @@ export = {
   plugins: ['import'],
   extends: ['plugin:@typescript-eslint/recommended'],
   rules: {
-    /**
-     * import/order does not sort members, use sort-imports to sort members
-     *
-     * @see https://github.com/import-js/eslint-plugin-import/issues/1670#issuecomment-1018833148
-     */
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-        allowSeparatedGroups: true,
-      },
-    ],
     '@typescript-eslint/consistent-type-imports': [
       'error',
       {
         prefer: 'type-imports',
       },
     ],
-    'import/first': ['error'],
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
-        pathGroups: [
-          {
-            pattern: '@/**',
-            group: 'internal',
-            position: 'before',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['builtin', 'object', 'type'],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-        },
-      },
-    ],
-    'import/newline-after-import': ['error'],
   },
 }
--- environments/es6.ts
+++ environments/es6.ts
@@ -8,5 +8,38 @@ export = {
   },
   rules: {
     'arrow-body-style': ['error', 'as-needed'],
+    /**
+     * import/order does not sort members, use sort-imports to sort members
+     *
+     * @see https://github.com/import-js/eslint-plugin-import/issues/1670#issuecomment-1018833148
+     */
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        allowSeparatedGroups: true,
+      },
+    ],
+    'import/first': ['error'],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin', 'object', 'type'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+    'import/newline-after-import': ['error'],
   },
 }
